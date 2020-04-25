4d3edac58e716cbed7b0e480f330ccb9377b7fbc
Chris Salgado <musicman785@gmail.com>
2020-04-25 14:39:22 -0700

    Finished js code and added all comments to js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,12 +12,19 @@ var answerA = document.getElementById("A");
 var answerB = document.getElementById("B");
 // target answer selection C 
 var answerC = document.getElementById("C");
-
+// targets loading page section
 var loadingPage = document.getElementById("loading-page");
-
+// targets the quiz section
 var quizEl = document.getElementById("begin-quiz");
-
+// targets final score section 
 var finalScore = document.getElementById("final-score");
+// DOM variable targets initials input
+var initials = document.getElementById("#initials");
+// DOM variable targets submit button
+var submit = document.getElementById("submit");
+
+var startOver = document.getElementById("start-again");
+
 
 
 // Question set for user
@@ -86,6 +93,9 @@ function setTime() {
         // condition to clear timer once it hits zero
         if (time === 0) {
             clearInterval(timerInterval);
+            quizEl.style.display = "none";
+            quizResults();
+
         }
 
     }, 1000);
@@ -100,61 +110,113 @@ function beginQuiz() {
 
 
 
-// function to take user to next question
+// function takes user to next question once anwser is recorded.
 
 function nextQuestion(event) {
-        console.log(currentQuesIndex);
 
-    // need a conditional statement to identify if user answers are correct or not. //
+
+    // condition uses click event to add to user score if correct ansser is made //
     if (event.target.value === questions[currentQuesIndex].correctAns) {
         score++;
+    } else {
+        time -= 5;
+        counter.textContent = time + " Seconds left on quiz.";
     }
 
+
+
+    // Condition executes next question
     if (currentQuesIndex < questions.length) {
         currentQuesIndex++;
-
+        // moves to next section once condition is met
     } if (currentQuesIndex === questions.length) {
 
         quizEl.style.display = "none";
-
+        // this function call takes user to results section
         quizResults();
         return;
 
     }
     showQuestion();
 }
+// score is added and shown
 function quizResults() {
     document.getElementById("score-counter").textContent = score;
     finalScore.style.display = "block";
 
 }
-
-// create a button that will capture the value of the of which answer the user clicks on. each time an answer is made. capture the value of answer A, B, C variables.
-answerA.addEventListener("click", nextQuestion)
-
-
-answerB.addEventListener("click", nextQuestion)
-
-answerC.addEventListener("click", nextQuestion)
+//click events for aswers to questions
+answerA.addEventListener("click", nextQuestion);
 
 
+answerB.addEventListener("click", nextQuestion);
+
+answerC.addEventListener("click", nextQuestion);
+
+// submit event for initial and score 
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var initToAdd = initials.value;
+
+    // variable holds score in number form when retrieved from local storage
+    var highScores = JSON.parse(localStorage.getItem("highScore"));
+    //conditional statement that set information to the local storage
+    if (highScores) {
+
+        highScores.push({
+            score: score,
+            initials: initToAdd
+        });
+
+        localStorage.setItem("highScore", JSON.stringify(highScores));
+    } else {
+        localStorage.setItem("highScore", JSON.stringify([{
+            score: score,
+            initials: initToAdd
+        }]));
+    }
+
+
+    // close section of 'finalscore' and move to next section of page
+    finalScore.style.display = "none";
+    getHighScore();
+
+
+})
+
+// Use an empty variable to manipulate DOM element
+var targetElement;
+// use function to target and diplay to page
+function getHighScore() {
+    targetElement = document.getElementById("high-score");
+    targetElement.style.display = "block";
+    // same variable again to retrieve information as a number
+    var highScores = JSON.parse(localStorage.getItem("highScore"));
+
+    // loop sets numbers in order of entry and displays them.
+    if (highScores) {
+        for (var i = 0; i < highScores.length; i++) {
+            // DOM variable to show input using the .textContext keyword
+            var paragraph = document.createElement("p");
+            //scores and names are recorded into paragraph element
+            paragraph.textContent = highScores[i].initials + ": " + highScores[i].score;
+            //appending input to element
+            targetElement.appendChild(paragraph);
+        }
+
+    }
 
 
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-// questions will have 5 seconds to be answered
-// var quesTimeLimit = 5;
-
+// Using this funtion to restart quiz when button is clicked
+function refreshPage() {
+    window.location.reload();
+}//added function to in case user wants a empty score tally
+function clearPage() {
+    window.location.reload();
+    localStorage.clear();
+}
 
